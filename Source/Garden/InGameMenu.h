a95c7fbdfec0f75faf7fc821864ef225edf2171d
Sergio Kirienko Panea <s.kirienkopanea@student.tudelft.nl>
2021-12-23 18:44:32 +0100

    In game menu

---
+++ Source/Garden/InGameMenu.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuInterface.h"
+#include "InGameMenu.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class GARDEN_API UInGameMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetMenuInterface(IMenuInterface* MI);
+	void Setup();
+	UFUNCTION()
+	void Teardown();
+
+protected:
+	virtual bool Initialize() override;
+private:
+
+	//The names of these variables must match the names of the WBP Blueprint menu buttons/fields/etc (widgets)
+	UPROPERTY(meta = (BindWidget))
+	class UButton* DisconnectButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* CancelButton;
+
+	IMenuInterface* MenuInterface;
+
+
+};
